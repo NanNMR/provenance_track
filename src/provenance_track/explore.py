443fdbce358e6a770a9c63b0a63b23d812ac9652
Gerard Weatherby <gweatherby@uchc.edu>
2024-01-02 17:47:40 -0500

    expand

--- src/provenance_track/explore.py
+++ src/provenance_track/explore.py
@@ -1,14 +1,23 @@
 from typing import Any
 from pathlib import Path
+import inspect
 import os
 
 
+def _showdoc(x,out):
+    if (d := getattr(x,'__doc__',None)) is not None:
+        print(d,file=out)
+
 def _explore(thing:Any,out):
+#    print(thing.__name__,file=out)
     print(type(thing),file=out)
+    _showdoc(thing,out)
     if (fa := getattr(thing,'__file__',None)) is not None:
         print(f"file: {fa}",file=out)
     for d in dir(thing):
         print(d,file=out)
+    for n,v in inspect.getmembers(thing):
+        print(f"{n} is {v}",file=out)
 
 
 
