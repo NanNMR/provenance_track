d3d8f93822eb1109efc99221ab3c51462495c8d9
Gerard Weatherby <gweatherby@uchc.edu>
2024-02-09 08:20:37 -0500

    Truncate trigger

--- provenance_functions.py
+++ provenance_functions.py
@@ -1,5 +1,3 @@
-import provenance_track.logsetup
-
 CREATE LANGUAGE plpython3u;
 
 CREATE or REPLACE FUNCTION public.record_provenance()
@@ -12,8 +10,14 @@ import provenance_track
 from provenance_track import provenance_track_logger, record
 plpy.info(provenance_track.__version__)
 record(plpy,TD)
-
 return "OK"
+$$;
+
+CREATE or REPLACE FUNCTION public.dont_truncate_provenance()
+  RETURNS TRIGGER
+LANGUAGE plpython3u 
+AS $$ 
+raise ValueError(f"truncate of provenance tracked table {TD['table_schema']}.{TD['table_name']} not supported. Use delete.")
 $$;
 
 
@@ -24,7 +28,9 @@ import provenance_track
 provenance_track.logsetup.set_loglevel(level)
 $$;
 
-CREATE or REPLACE FUNCTION public.get_provenance_loglevel()
+
+CREATE or REPLACE FUNCTION public.get_provenance_loginfo()
+  RETURNS TEXT
 LANGUAGE plpython3u 
 AS $$ 
 import provenance_track 
--- trigger_pdata.py
+++ trigger_pdata.py
@@ -1,24 +1,4 @@
-import provenance_track.logsetup
-
 CREATE LANGUAGE plpython3u;
-
-CREATE or REPLACE FUNCTION public.record_ch() 
-  RETURNS TRIGGER
-LANGUAGE plpython3u 
-AS $$ 
-import datetime 
-import plpy
-import provenance_track 
-from provenance_track import provenance_track_logger, record
-plpy.warning('trigger fire')
-plpy.info(provenance_track.__version__)
-provenance_track.logsetup.set_loglevel('DEBUG')
-record(plpy,TD)
-
-#plpy.info(provenance_track.nan_user())
-
-return "OK"
-$$;
 
 CREATE or REPLACE FUNCTION public.do_query() 
   RETURNS TRIGGER
@@ -33,7 +13,12 @@ $$;
 DROP TRIGGER pdata_trigger on public.pdata;
 CREATE TRIGGER pdata_trigger 
 BEFORE
-INSERT or UPDATE or DELETE or TRUNCATE
-ON public.pdata FOR EACH ROW EXECUTE PROCEDURE public.record_ch() 
---ON public.pdata FOR EACH ROW EXECUTE PROCEDURE public.do_query()
+INSERT or UPDATE or DELETE 
+ON public.pdata FOR EACH ROW EXECUTE PROCEDURE public.record_provenance() ;
+
+DROP TRIGGER pdata_truncate on public.pdata;
+CREATE TRIGGER pdata_truncate
+BEFORE
+TRUNCATE
+ON public.pdata EXECUTE PROCEDURE public.dont_truncate_provenance();
 
