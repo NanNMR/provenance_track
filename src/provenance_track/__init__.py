681551d212c17d2f776cdfda537cb9088fdd311d
Gerardwx <gerardw@alum.mit.edu>
2024-02-26 09:29:06 -0500

    kill switch (missing file)

--- src/provenance_track/__init__.py
+++ src/provenance_track/__init__.py
@@ -1,8 +1,11 @@
 
 import importlib.metadata 
 import logging
-from pathlib import Path
 import sys
+from pathlib import Path
+from provenance_track.plpyapi import PyResult,PlpyAPI
+from provenance_track.logsetup import setup_logging, set_loglevel,get_loginfo
+from provenance_track.provenance import nan_user, record, disable_provenance
 
 __version__ =  importlib.metadata.version('provenance_track')
 
@@ -12,9 +15,6 @@ DISABLE_SENTINEL = Path('/var/tmp/noprovenance')
 
 
 provenance_track_logger = logging.getLogger(__name__)
-from provenance_track.plpyapi import PyResult,PlpyAPI
-from provenance_track.logsetup import setup_logging, set_loglevel,get_loginfo
-from provenance_track.provenance import nan_user, record, disable_provenance
 
 setup_logging()
 provenance_track_logger.warning(f'loading {sys.version_info}')
