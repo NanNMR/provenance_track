e3efb1210a306bc93b89d817925d64fdd888bc7e
Gerardwx <gerardw@alum.mit.edu>
2024-02-27 08:42:16 -0500

    fix imports

--- src/provenance_track/__init__.py
+++ src/provenance_track/__init__.py
@@ -3,9 +3,6 @@ import importlib.metadata
 import logging
 import sys
 from pathlib import Path
-from provenance_track.plpyapi import PyResult,PlpyAPI
-from provenance_track.logsetup import setup_logging, set_loglevel,get_loginfo
-from provenance_track.provenance import nan_user, record, disable_provenance
 
 __version__ =  importlib.metadata.version('provenance_track')
 
@@ -16,6 +13,9 @@ DISABLE_SENTINEL = Path('/var/tmp/noprovenance')
 
 provenance_track_logger = logging.getLogger(__name__)
 
+from provenance_track.plpyapi import PyResult,PlpyAPI
+from provenance_track.logsetup import setup_logging, set_loglevel,get_loginfo
+from provenance_track.provenance import nan_user, record, disable_provenance
 setup_logging()
 provenance_track_logger.warning(f'loading {sys.version_info}')
 
